95e8f8ebbdcb017516b9f61636fafe08c5832269
aquilino <hidro23@hotmail.com>
2022-01-14 16:32:49 +0100

    Update telegramApi.py

--- telegramApi.py
+++ telegramApi.py
@@ -6,7 +6,7 @@ class TelegramApi():
   def __init__(self, token):
     self._token = token
   
-  def sendmessage(self, chat_id, message, reply_markup=None):
+  def send_message(self, chat_id, message, reply_markup=None):
         url = BOT_URL.format(self._token) + '/sendMessage'
         data = {'chat_id': chat_id, 'text': message, 'parse_mode': 'HTML'}
         if reply_markup:
