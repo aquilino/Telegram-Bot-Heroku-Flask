29ed480c3257b40c1ffab8aafaf0b1a0e8fedc93
aquilino <hidro23@hotmail.com>
2022-01-14 13:53:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import requests  
 import os
-from flask import Flask, request
+from flask import Flask, request, json
 
 BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'  # <-- add your telegram token as environment variable
 
@@ -9,16 +9,27 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
+
+def answer(word):
+    api = "https://api.coingecko.com/api/v3/coins/{word}"
+    json_data = json.loads(api.content)
+    //print(api.content)
+    market_data = json_data['market_data']['current_price']['eur']
+    market_cap = json_data['market_data']['market_cap']['eur']
+    links = json_data['links']['homepage'][0]
+    symbol= json_data['symbol']
+    return f'Symbol: {symbol}\nCurrent_price: {market_data}€\nMarket_cap: {market_cap}€\nOfficial_website: {links}'
+
 def main():  
     data = request.json
 
     print(data)  # Comment to hide what Telegram is sending you
     chat_id = data['message']['chat']['id']
     message = data['message']['text']
-
+   
     json_data = {
         "chat_id": chat_id,
-        "text": message,
+        "text":  answer(message),
     }
 
     message_url = BOT_URL + 'sendMessage'
