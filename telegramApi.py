d58b713331d01833dbc02923ac70ee8b8e154abf
aquilino <hidro23@hotmail.com>
2022-01-14 15:43:12 +0100

    Update telegramApi.py

--- telegramApi.py
+++ telegramApi.py
@@ -1,14 +1,14 @@
-import requests, os
+import requests
 
-BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'  # <-- add your telegram token as environment variable
+BOT_URL = 'https://api.telegram.org/bot{}'  # <-- add your telegram token as environment variable
 
 class TelegramApi():
   def __init__(self, token):
     self._token = token
   
   def sendMessage(self, chat_id, message, reply_markup=None):
-        url = BOT_URL + 'sendMessage'
-        data = {'chat_id': chat_id, 'text': message, 'parse_mode': HTML}
+        url = BOT_URL.format(self._token) + '/sendMessage'
+        data = {'chat_id': chat_id, 'text': message, 'parse_mode': 'HTML'}
         if reply_markup:
           data['reply_markup'] = reply_markup
         r = requests.post(url, data=data)
