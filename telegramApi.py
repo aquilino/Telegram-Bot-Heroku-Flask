710673ead382817a1cb90fe0c7a12c316fb4d8d0
aquilino <hidro23@hotmail.com>
2022-01-16 23:31:09 +0100

    Update telegramApi.py

--- telegramApi.py
+++ telegramApi.py
@@ -5,7 +5,6 @@ BOT_URL = 'https://api.telegram.org/bot{}'  # <-- add your telegram token as env
 class TelegramApi():
   def __init__(self, token):
     self._token = token
-    self._channel =  channel
   
   def send_message(self, chat_id, message, reply_markup=None):
         url = BOT_URL.format(self._token) + '/sendMessage'
@@ -13,15 +12,6 @@ class TelegramApi():
         if reply_markup:
           data['reply_markup'] = reply_markup
         r = requests.post(url, data=data)
-        if r.status_code == 200:
-          return r.json()
-        return None
-  
-  def send_chat_action(self, action=None, chat_id=None):
-        chat_id = chat_id if chat_id else self._channel
-        url = f"https://api.telegram.org/bot{self._token}/sendChatAction"
-        data = {"chat_id": chat_id, "action": action} 
-        r = requets.get(url, data=data)
         if r.status_code == 200:
           return r.json()
         return None
