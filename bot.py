7e7926bf3ba9349b6558797f83153e773f7c16b2
aquilino <hidro23@hotmail.com>
2022-01-16 22:19:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,6 +48,21 @@ def main():
         text = payload["message"]["text"]
         message = answer(text)
         telegramApi.send_message(chat_id, message)
+        elif payload["message"]["text"] == "/distro":
+            message = "Selecciona tu distro"
+            buttons = []
+            buttons.append({"text": "Escritorio",
+                           "callback_data": "desktop"})
+            buttons.append({"text": "Servidor",
+                           "callback_data": "server"})
+            inline_keyboard = json.dumps({"inline_keyboard": [buttons]})
+            telegramApi.send_message(chat_id, message, inline_keyboard)
+        elif "callback_query" in payload:
+            chat_id = payload["callback_query"]["message"]["chat"]["id"]
+            if payload["callback_query"]["data"] == "desktop":
+                telegramApi.send_message(chat_id, "Has elegido escritorio")
+            if payload["callback_query"]["data"] == "server":
+                telegramApi.send_message(chat_id, "Has elegido servidor")
     return 'OK', 201
 
 
