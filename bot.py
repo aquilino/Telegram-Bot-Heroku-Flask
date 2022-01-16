38ac8ebf2c6478fbd2413e469b3e77de09d1d24d
aquilino <hidro23@hotmail.com>
2022-01-16 18:21:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,10 +19,10 @@ def answer(word):
     api = requests.get(f'{page}{word}')
     json_data = json.loads(api.content)
     logger(json.dumps(json_data, indent=4, sort_keys=True))
-    market_data = json_data["market_data"]["current_price"]["eur"]
-    market_cap = json_data["market_data"]["market_cap"]["eur"]
-    links = json_data["links"]["homepage"][0]
-    symbol= json_data["symbol"]
+#    market_data = json_data["market_data"]["current_price"]["eur"]
+#    market_cap = json_data["market_data"]["market_cap"]["eur"]
+#    links = json_data["links"]["homepage"][0]
+#    symbol= json_data["symbol"]
 #    try:
 #        if "error" or not json_data:
 #             msg = f"Symbol: {symbol}\nCurrent_price: {market_data}€\nMarket_cap: {market_cap}€\nOfficial_website: {links}"
