906bc9c90df102556e65136184040b1b3542af42
aquilino <hidro23@hotmail.com>
2022-01-16 14:25:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ def get_status():
     return 'Up and running', 201
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/{}'.os.environ["BOT_KEY"], methods=['GET', 'POST'])
 def main():
     try:
         if request.method == 'GET' or not request.json:
